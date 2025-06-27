3b197ff0721119570f3da63785aad489bbf57c52
Ayumu3746221 <a.kkt0307@gmail.com>
2025-06-27 12:24:00 +0900

    fix from code review

--- todo-api/services/tasks/changeActionState.ts
+++ todo-api/services/tasks/changeActionState.ts
@@ -1,7 +1,7 @@
 import prisma from "~/prisma.js";
 
 export const changeActionState = async (taskId: number, isAction: boolean) => {
-  prisma.task.update({
+  await prisma.task.update({
     where: { id: taskId },
     data: {
       isAction: {
